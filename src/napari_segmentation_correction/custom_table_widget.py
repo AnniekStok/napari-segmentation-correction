1d0884037e406ca6cdbd9a554c46d6a2ca575583
AnniekStok <anniek.stokkermans@gmail.com>
2025-09-24 16:32:47 +0200

    use separate colored table widget to display label properties

--- src/napari_segmentation_correction/custom_table_widget.py
+++ src/napari_segmentation_correction/custom_table_widget.py
@@ -1,29 +1,99 @@
+import napari
 import pandas as pd
 from matplotlib.colors import to_rgb
-from napari_skimage_regionprops import TableWidget
 from qtpy.QtGui import QColor
+from qtpy.QtWidgets import (
+    QFileDialog,
+    QHBoxLayout,
+    QPushButton,
+    QStyledItemDelegate,
+    QTableWidget,
+    QTableWidgetItem,
+    QVBoxLayout,
+    QWidget,
+)
 
 
-class ColoredTableWidget(TableWidget):
-    """Customized table widget based on the napari_skimage_regionprops TableWidget"""
+class FloatDelegate(QStyledItemDelegate):
+    def __init__(self, decimals, parent=None):
+        super().__init__(parent)
+        self.nDecimals = decimals
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def displayText(self, value, locale):
+        try:
+            number = float(value)
+        except (ValueError, TypeError):
+            return str(value)
 
+        if number.is_integer():
+            return str(int(number))
+        return f"{number:.{self.nDecimals}f}"
+
+
+class ColoredTableWidget(QWidget):
+    """Customized table widget with colored rows based on label colors in a napari Labels layer"""
+
+    def __init__(self, layer: "napari.layers.Layer", viewer: "napari.Viewer" = None):
+        super().__init__()
+
+        self._layer = layer
+        self._viewer = viewer
+        self._table_widget = QTableWidget()
+
+        self._layer.events.colormap.connect(self._set_label_colors_to_rows)
+        if hasattr(layer, "properties"):
+            self._set_data(layer.properties)
+        else:
+            self._set_data({})
         self.ascending = False  # for choosing whether to sort ascending or descending
 
         # Reconnect the clicked signal to your custom method.
-        self._view.clicked.connect(self._clicked_table)
+        self._table_widget.clicked.connect(self._clicked_table)
 
         # Connect to single click in the header to sort the table.
-        self._view.horizontalHeader().sectionClicked.connect(self._sort_table)
+        self._table_widget.horizontalHeader().sectionClicked.connect(self._sort_table)
 
+        copy_button = QPushButton("Copy to clipboard")
+        copy_button.clicked.connect(self._copy_table)
+
+        save_button = QPushButton("Save as csv")
+        save_button.clicked.connect(self._save_table)
+
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(copy_button)
+        button_layout.addWidget(save_button)
+        main_layout = QVBoxLayout()
+        main_layout.addLayout(button_layout)
+        main_layout.addWidget(self._table_widget)
+        self.setLayout(main_layout)
         self.setMinimumHeight(300)
+
+    def _set_data(self, table: dict) -> None:
+        """Set the content of the table from a dictionary"""
+
+        self._table = table
+        self._layer.properties = table
+
+        self._table_widget.clear()
+        try:
+            self._table_widget.setRowCount(len(next(iter(table.values()))))
+            self._table_widget.setColumnCount(len(table))
+        except StopIteration:
+            pass
+
+        for i, column in enumerate(table):
+            self._table_widget.setHorizontalHeaderItem(i, QTableWidgetItem(column))
+            for j, value in enumerate(table.get(column)):
+                self._table_widget.setItem(j, i, QTableWidgetItem(str(value)))
+
+        self._table_widget.setItemDelegate(FloatDelegate(3, self._table_widget))
+
+        self._set_label_colors_to_rows()
 
     def _set_label_colors_to_rows(self) -> None:
         """Apply the colors of the napari label image to the table"""
 
-        for i in range(self._view.rowCount()):
+        for i in range(self._table_widget.rowCount()):
             label = self._table["label"][i]
             label_color = to_rgb(self._layer.get_color(label))
             scaled_color = (
@@ -31,42 +101,40 @@ class ColoredTableWidget(TableWidget):
                 int(label_color[1] * 255),
                 int(label_color[2] * 255),
             )
-            for j in range(self._view.columnCount()):
-                self._view.item(i, j).setBackground(QColor(*scaled_color))
+            for j in range(self._table_widget.columnCount()):
+                self._table_widget.item(i, j).setBackground(QColor(*scaled_color))
+
+    def _save_table(self) -> None:
+        """Save table to csv file"""
+        filename, _ = QFileDialog.getSaveFileName(self, "Save as csv", ".", "*.csv")
+        pd.DataFrame(self._table).to_csv(filename)
+
+    def _copy_table(self) -> None:
+        """Copy table to clipboard"""
+        pd.DataFrame(self._table).to_clipboard()
 
     def _clicked_table(self):
-        """Also set show_selected_label to True and jump to the corresponding stack position"""
+        """Set the current viewer dims to the label that was clicked on."""
 
-        super()._clicked_table()
-        self._layer.show_selected_label = True
+        row = self._table_widget.currentRow()
+        spatial_columns = sorted([key for key in self._table if "centroid" in key])
+        spatial_coords = [int(self._table[col][row]) for col in spatial_columns]
 
-        row = self._view.currentRow()
-        current_step = self._viewer.dims.current_step
-        z = int(self._table["centroid-0"][row])
-        if len(current_step) == 4:
-            if "time_point" in self._table:
-                t = int(self._table["time_point"][row])
-                new_step = (t, z, current_step[2], current_step[3])
-            else:
-                new_step = (z, current_step[1], current_step[2])
-        elif len(current_step) == 3:
-            if "time_point" in self._table:
-                t = int(self._table["time_point"][row])
-                new_step = (t, current_step[1], current_step[2])
-            else:
-                new_step = (z, current_step[1], current_step[2])
+        if "time_point" in self._table:
+            t = int(self._table["time_point"][row])
+            new_step = (t, *spatial_coords)
         else:
-            new_step = current_step
+            new_step = spatial_coords
         self._viewer.dims.current_step = new_step
 
     def _sort_table(self):
         """Sorts the table in ascending or descending order"""
 
-        selected_column = list(self._table.keys())[self._view.currentColumn()]
+        selected_column = list(self._table.keys())[self._table_widget.currentColumn()]
         df = pd.DataFrame(self._table).sort_values(
             by=selected_column, ascending=self.ascending
         )
         self.ascending = not self.ascending
 
-        self.set_content(df.to_dict(orient="list"))
+        self._set_data(df.to_dict(orient="list"))
         self._set_label_colors_to_rows()
